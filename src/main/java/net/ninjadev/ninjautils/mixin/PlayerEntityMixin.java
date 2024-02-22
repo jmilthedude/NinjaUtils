ab0b7fbf2c02e72e327ca6ca6dcf1a3daa181516
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2024-02-22 15:29:26 -0600

    remap false

--- src/main/java/net/ninjadev/ninjautils/mixin/PlayerEntityMixin.java
+++ src/main/java/net/ninjadev/ninjautils/mixin/PlayerEntityMixin.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
 
-    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList()Ljava/util/ArrayList;", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList()Ljava/util/ArrayList;", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD, remap = false)
     public void preEntityCollision(CallbackInfo ci, float f, Box box, List<Entity> list) {
         ModEvents.PLAYER_ENTITY_COLLISION.invoke(new PlayerEntityCollisionEvent.Data((PlayerEntity)(Object)this, list));
     }
