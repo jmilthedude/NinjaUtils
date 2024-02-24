ea495978dc3554b440ca5e282380e1ec53d75bc8
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2024-02-24 08:39:18 -0600

    player name color for embedded sleep messages

--- src/main/java/net/ninjadev/ninjautils/compat/DiscordIntegrationCompat.java
+++ src/main/java/net/ninjadev/ninjautils/compat/DiscordIntegrationCompat.java
@@ -6,15 +6,18 @@ import de.erdbeerbaerlp.dcintegration.common.util.DiscordMessage;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.ninjadev.ninjautils.data.NameColorState;
 
+import java.awt.*;
 import java.util.UUID;
 
 public class DiscordIntegrationCompat {
 
     public void sendMessage(ServerPlayerEntity player, Text text) {
         final String avatarURL = Configuration.instance().webhook.playerAvatarURL.replace("%uuid%", player.getUuid().toString()).replace("%uuid_dashless%", player.getUuid().toString().replace("-", "")).replace("%name%", player.getName().getString()).replace("%randomUUID%", UUID.randomUUID().toString());
-
+        Color color = NameColorState.get().getPlayerColor(player);
         final EmbedBuilder b = Configuration.instance().embedMode.playerLeaveMessages.toEmbed()
+                .setColor(color)
                 .setDescription("*" + text.getString() + "*");
         DiscordIntegration.INSTANCE.sendMessage(new DiscordMessage(b.build()), DiscordIntegration.INSTANCE.getChannel(Configuration.instance().advanced.serverChannelID));
     }
